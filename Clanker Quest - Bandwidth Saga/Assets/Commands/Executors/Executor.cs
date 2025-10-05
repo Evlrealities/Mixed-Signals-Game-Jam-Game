e69dd88b6ef9004ed3a39779e9049bf27f217d85
Evlrealities <evlrealities@gmail.com>
2025-10-05 18:12:31 -0400

    broken but like almost there?

--- Clanker Quest - Bandwidth Saga/Assets/Commands/Executors/Executor.cs
+++ Clanker Quest - Bandwidth Saga/Assets/Commands/Executors/Executor.cs
@@ -1,10 +1,16 @@
-using TMPro.EditorUtilities;
+using System.Collections;
 using UnityEngine;
 
 public abstract class Executor : MonoBehaviour {
     public void Init(string[] inputWords) {
         Execute(inputWords);
-        Destroy(gameObject);
+        StartCoroutine(DestroyNextFrame());
+    }
+
+    IEnumerator DestroyNextFrame() {
+        // Wait one frame so the EventSystem/GraphicRaycaster finishes raycasts
+        yield return null;
+        if (this) Destroy(gameObject);
     }
 
     protected abstract void Execute(string[] inputWords);
